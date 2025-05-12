d8b950f7339e1eb1880bb63bde1cd91fae9e5b87
asher james <cabbageshell@protonmail.com>
2025-05-12 19:22:19 -0400

    inital commit

---
+++ vars/myStep.groovy
@@ -0,0 +1,11 @@
+def httpGet(String urlStr, Map headers = [:]) {
+    def url = new URL(urlStr)
+    def connection = url.openConnection()
+    connection.requestMethod = 'GET'
+    headers.each { k, v -> connection.setRequestProperty(k, v) }
+    connection.connect()
+    int code = connection.responseCode
+    def stream = (code >= 200 && code < 400) ? connection.inputStream : connection.errorStream
+    def response = stream?.text
+    [status: code, body: response]
+}
